760cf7baee5c467845a58497a9821b1a4be952fa
StefanijaFilipasikj <stefanija.filipashikj@students.finki.ukim.mk>
2023-11-08 18:43:20 +0100

    Create Pipe and Filter project

---
+++ Домашна 1/PipeAndFilter/src/main/java/ConvertCyrilicToLatinFilter.java
@@ -0,0 +1,7 @@
+public class ConvertCyrilicToLatinFilter implements Filter<String>{
+    @Override
+    public String execute(String input) {
+        //TODO implement logic
+        return input;
+    }
+}
